0e5aa363d621e8aa8987a2c85253e9957ff865c2
codebaker <codebaker404@gmail.com>
2025-02-26 22:30:08 +0100

    added file upload logic

--- src/routes/v1/ConservationRouter.ts
+++ src/routes/v1/ConservationRouter.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { isAuthenticated } from "../../middleware/authentication/authCheckMiddlewares";
 import MessageController from "@/controllers/v1/MessageController";
+import multer from "multer";
+import { multerStorage } from "@/utils/libs/upload/multerParams";
 
 export const ConversationRouter = express.Router();
 
@@ -9,9 +11,25 @@ ConversationRouter.get(
   isAuthenticated(),
   MessageController.fetchMessages
 );
+const messageAudioUploader = multer({
+  storage: multerStorage,
+  limits: {
+    files: 1,
+    fieldSize: 1000 * 1000 * 10, // 10 mega bytes
+  },
+});
+const messagesImageUploader = multer({
+  storage: multerStorage,
+  limits: {
+    files: 10,
+    fileSize: 1000 * 1000 * 5, // 5 mega bytes
+  },
+});
 
 ConversationRouter.post(
   "/api/v1/conversations/:conversationId/messages",
   isAuthenticated(),
+  messageAudioUploader.single("record"),
+  messagesImageUploader.array("images"),
   MessageController.postMessage
 );
